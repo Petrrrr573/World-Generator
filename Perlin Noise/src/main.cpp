e8267f8b72e86152750a678a9be4e21c11a86e3c
Petrrrr573 <65619514+Petrrrr573@users.noreply.github.com>
2024-07-12 16:26:29 +0200

    functional perlin noise with a colored map

--- Perlin Noise/src/main.cpp
+++ Perlin Noise/src/main.cpp
@@ -16,9 +16,152 @@
 #include <stdio.h>
 #include <SDL.h>
 
+#include <iostream>
+
 #if !SDL_VERSION_ATLEAST(2,0,17)
 #error This backend requires SDL 2.0.17+ because of SDL_RenderGeometry() function
 #endif
+
+
+typedef struct {
+    float x, y;
+} vector2;
+
+float interpolate(float a0, float a1, float w) {
+    return (a1 - a0) * (3.0 - w * 2.0) * w * w + a0;
+}
+
+vector2 randomGradient(float ix, float iy, int SEED) {
+    // No precomputed gradients mean this works for any number of grid coordinates
+    const unsigned w = 8 * sizeof(unsigned);
+    const unsigned s = w / 2;
+    unsigned a = ix, b = iy;
+    a *= 3284157443*SEED;
+
+    b ^= a << s | a >> w - s;
+    b *= 1911520717*SEED;
+
+    a ^= b << s | b >> w - s;
+    a *= 2048419325*SEED;
+    float random = a * (3.14159265 / ~(~0u >> 1)); // in [0, 2*Pi]
+
+    // Create the vector from the angle
+    vector2 v;
+    v.x = sin(random);
+    v.y = cos(random);
+
+    return v;
+}
+
+float dotGridGradient(int ix, int iy, float x, float y, float SEED) {
+    vector2 gradient = randomGradient(ix, iy, SEED);
+
+    float dx = x - (float)ix;
+    float dy = y - (float)iy;
+
+    // Compute the dot-product
+    return (dx * gradient.x + dy * gradient.y);
+}
+
+float perlin(float x, float y, int SEED) {
+    int x0 = (int)x;
+    int y0 = (int)y;
+    int x1 = x0 + 1;
+    int y1 = y0 + 1;
+
+    float sx = x - (float)x0;
+    float sy = y - (float)y0;
+
+    float n0, n1, ix0, ix1, value;
+
+    n0 = dotGridGradient(x0, y0, x, y, SEED);
+    n1 = dotGridGradient(x1, y0, x, y, SEED);
+    ix0 = interpolate(n0, n1, sx);
+
+    n0 = dotGridGradient(x0, y1, x, y, SEED);
+    n1 = dotGridGradient(x1, y1, x, y, SEED);
+    ix1 = interpolate(n0, n1, sx);
+
+    value = interpolate(ix0, ix1, sy);
+    return value;
+}
+
+void genPerlin(float GRID_SIZE, Uint8* pixels, bool showColor, int octaves, int SEED) {
+
+    for (int x = 0; x < 400; x++)
+    {
+        for (int y = 0; y < 400; y++)
+        {
+            int index = (y * 400 + x) * 4;
+
+
+            float val = 0;
+
+            float freq = 1;
+            float amp = 1;
+
+            for (int i = 0; i < octaves; i++)
+            {
+                val += perlin(x * freq / GRID_SIZE, y * freq / GRID_SIZE, SEED) * amp;
+
+                freq *= 2;
+                amp /= 2;
+
+            }
+
+            // Contrast
+            val *= 1.2;
+
+            // Clipping
+            if (val > 1.0f)
+                val = 1.0f;
+            else if (val < -1.0f)
+                val = -1.0f;
+
+            // Convert 1 to -1 into 255 to 0
+            int color = (int)(((val + 1.0f) * 0.5f) * 255);
+
+            // Set pixel color
+            pixels[index] = 255;
+            pixels[index + 1] = color;
+            pixels[index + 2] = color;
+            pixels[index + 3] = color;
+
+            if (showColor) {
+                if (val > -0.3 && val < 0) {
+                    pixels[index + 1] = 0;
+                    pixels[index + 2] = 200;
+                    pixels[index + 3] = 0;
+                }
+                else if (val > 0.7) {
+                    pixels[index + 1] = 240;
+                    pixels[index + 2] = 240;
+                    pixels[index + 3] = 240;
+                }
+                else if (val < 0.7 && val > 0.5) {
+                    pixels[index + 1] = 80;
+                    pixels[index + 2] = 80;
+                    pixels[index + 3] = 80;
+                }
+                else if (val < 0.5 && val > 0) {
+                    pixels[index + 1] = 0;
+                    pixels[index + 2] = 180;
+                    pixels[index + 3] = 180;
+                }
+                else if (val < -0.3 && val > -0.5) {
+                    pixels[index + 1] = 0;
+                    pixels[index + 2] = 200;
+                    pixels[index + 3] = 200;
+                }
+                else if (val<-0.5){
+                    pixels[index + 1] = 200;
+                    pixels[index + 2] = 0;
+                    pixels[index + 3] = 0;
+                }
+            }
+        }
+    }
+}
 
 // Main code
 int main(int, char**)
@@ -74,6 +217,19 @@ int main(int, char**)
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
+    Uint8* pixels = new Uint8[400 * 400 * 4];
+
+    int octaves = 12;
+    int GRID_SIZE = 200;
+    bool showColor = false;
+    int SEED = 1;
+
+    genPerlin(GRID_SIZE, pixels, showColor, octaves, SEED);
+
+
+    SDL_Texture* texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, 400, 400);
+    SDL_UpdateTexture(texture, nullptr, pixels, 400 * 4);
+
     // Main loop
     bool done = false;
     while (!done)
@@ -106,11 +262,17 @@ int main(int, char**)
             ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
             ImGui::Checkbox("Another Window", &show_another_window);
 
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+            ImGui::SliderInt("Grid size", &GRID_SIZE, 1, 400);  
+            ImGui::SliderInt("Octaves", &octaves, 1, 12);
+            ImGui::SliderInt("SEED", &SEED, 1, 10000);
             ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
-            if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
+            ImGui::Checkbox("Colors", &showColor);
+            if (ImGui::Button("Reload")) {
+                genPerlin(GRID_SIZE, pixels, showColor, octaves, SEED);
+                SDL_UpdateTexture(texture, nullptr, pixels, 400 * 4);
+                std::cout << GRID_SIZE << std::endl;
+            }
             ImGui::SameLine();
             ImGui::Text("counter = %d", counter);
 
@@ -132,6 +294,11 @@ int main(int, char**)
         SDL_RenderSetScale(renderer, io.DisplayFramebufferScale.x, io.DisplayFramebufferScale.y);
         SDL_SetRenderDrawColor(renderer, (Uint8)(clear_color.x * 255), (Uint8)(clear_color.y * 255), (Uint8)(clear_color.z * 255), (Uint8)(clear_color.w * 255));
         SDL_RenderClear(renderer);
+
+
+        SDL_Rect rect = { 0, 0, 400, 400 };
+        SDL_RenderCopy(renderer, texture, nullptr, &rect);
+
         ImGui_ImplSDLRenderer2_RenderDrawData(ImGui::GetDrawData(), renderer);
         SDL_RenderPresent(renderer);
     }
